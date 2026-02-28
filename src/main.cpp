27e4fbc5e02db57dd6c91ee6de221172b2326e07
AbhishekShrivastavaa <abhi123456shri@gmail.com>
2026-02-28 21:53:10 +0530

    Implement TTP223 touch detection logic and update metadata

--- src/main.cpp
+++ src/main.cpp
@@ -2,43 +2,44 @@
 /**
  * @file main.ino
  * @brief Embedded Touch Detection System using TTP223
- * @author YOUR_NAME
- * @date YYYY-MM-DD
+ * @author Abhishek Shrivastava
+ * @date 2026-02-28
  *
  * @details
  * Reads digital touch input from TTP223 sensor
- * and displays structured output via Serial Monitor.
+ * and displays structured ouput via Serial Monitor.
  */
 
- // TODO 1:
- // Define touch sensor digital pin (Use pin 2)
+// Define touch sensor digital pin (Use pin 2)
+#define TOUCH_PIN 2
 
- // TODO 2:
- // Create variable to store touch state
+// Create variable to store touch state
+int touchState = 0;
 
 void setup() {
 
-    // TODO 3:
     // Initialize Serial communication (9600 baud rate)
+    Serial.begin(9600);
 
-    // TODO 4:
     // Configure touch pin as INPUT
+    pinMode(TOUCH_PIN, INPUT);
 
-    // TODO 5:
     // Print system initialization message
+    Serial.println("TTP223 Touch Detection System Initialized");
 }
 
 void loop() {
 
-    // TODO 6:
     // Read digital value from touch sensor
+    touchState = digitalRead(TOUCH_PIN);
 
-    // TODO 7:
     // If touch detected (HIGH)
-    //     Print "Touch Detected"
-    // Else
-    //     Print "No Touch"
+    if (touchState == HIGH) {
+        Serial.println("Touch Detected");
+    } else {
+        Serial.println("No Touch");
+    }
 
-    // TODO 8:
-    // Add small delay (200–500ms)
+    // Add small delaay (200–500ms)
+    delay(300);
 }
